f6bdac808a53aede629ba73bbcce224672dc326f
najim el guennouni <60970889+najim-el-guennouni@users.noreply.github.com>
2023-09-04 23:13:53 +0100

    start

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './../node_modules/bootstrap';
+import './assets/main.css';
+
+
+import { createApp } from 'vue';
+import App from './App.vue';
+
+createApp(App).mount('#app');
