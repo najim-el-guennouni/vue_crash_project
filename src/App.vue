87c7b99fd32e12d91d06287acfa6b3bbf6330257
najim el guennouni <60970889+najim-el-guennouni@users.noreply.github.com>
2023-09-10 23:24:07 +0100

    add_aboute-route

--- src/App.vue
+++ src/App.vue
@@ -15,12 +15,14 @@
       :tasks="tasks"
     />
     
+    <router-view></router-view>
+    
     <Footer />
   </div>
 </template>
 <script>
 import Header from './components/Header.vue';
-import Footer from './components/Footer.vue';
+import Footer from './components/Footer.vue'
 import Tasks from './components/Tasks.vue';
 import AddTask from './components/AddTask.vue';
 export default {
--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
-import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './../node_modules/bootstrap';
-import './assets/main.css';
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
 
-import { createApp } from "vue";
-import App from "./App.vue";
-
-
-
-createApp(App).mount("#app");
+createApp(App)
+  .use(router)
+  .mount('#app')
